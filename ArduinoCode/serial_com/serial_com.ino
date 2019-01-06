cba5caffa03dab1c36c83a848707fe9ee1db20a9
Kiet <akiet00@yahoo.com>
2019-01-05 20:23:14 -0600

    save changes

--- ArduinoCode/serial_com/serial_com.ino
+++ ArduinoCode/serial_com/serial_com.ino
@@ -1,21 +1,43 @@
-
-
 const uint8_t bufferSize = 5;
 uint8_t buffer[bufferSize];
 
+bool SERIAL_TEST_FLAG = true;
+#define MOTOR1 1
+#define MOTOR2 2
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(2000000);
+  pinMode(MOTOR1, OUTPUT);
+  pinMode(MOTOR2, OUTPUT);
+  handshake();
 }
 
 void loop() {
-  Serial.println("Ready");
-  if( Serial.available())
+  if( Serial.available()>0)
   {
-    char cmd = Serial.read();
+    if( SERIAL_TEST_FLAG)
+    {
+      Serial.println("Ready");
+    }
+    Serial.println(Serial.read());//send back to the sender
   }
 }
+void handshake()
+{
+  while(Serial.available()<=0)
+  {
+    Serial.println("Waiting for commands ...");
+    delay(100);
+  }
+}
+void accelerate_ramp(double acceleration_time)
+{
+  
+}
 
-uint8_t checksum(){
+/*Checksum to verify data content sent through serial port*/
+uint8_t checksum()
+{
   uint8_t result = 0;
   uint16_t sum = 0;
   
--- PullTestGui/arduino_serial_test.py
+++ PullTestGui/arduino_serial_test.py
@@ -6,23 +6,31 @@ import sys
 
 class ArduinoSerialCom:
 	def __init__(self, portname, baudrate):
-		self.ser = serial.Serial(portname, baudrate)
-		self.ser.flush()
-
-		#handshake with the Arduino
-	def write2Arduino(data):
+		self.ser = serial.Serial(port=portname, baudrate=baudrate, timeout=0.005)
+		self.ser.flushInput()
+	def write2Arduino(self,data):
 		self.ser.write(data)
-	def readStream():
+	def readStream(self):
 		data = self.ser.readline()
 		print(data)
 		return data
-
+	def close(self):
+		self.ser.close()
 def main():
 	portname = input("Enter the Arduino port name: ").upper()
 	baudrate = int(input("Enter the baudrate: "))
 	ard_com = ArduinoSerialCom(portname, baudrate)
 
+	data = 'R'
 	print('Test sending data')
-
-
+	try:
+		while True:
+			ard_com.readStream()
+			time.sleep(2)
+			print("Sending data ... %s" %data.encode())
+			ard_com.write2Arduino(b'R')
+			ard_com.ser.flush()
+	except KeyboardInterrupt:
+		ard_com.close()
+		print('exiting ... ')
 main()
